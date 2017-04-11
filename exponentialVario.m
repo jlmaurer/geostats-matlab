6ef8d22eacbced0caa5b5503f58c02b14f8630d2
jlmaurer <maurer.jeremy@gmail.com>
2017-04-10 22:32:33 -0700

    Clean up exponentialVario

--- exponentialVario.m
+++ exponentialVario.m
@@ -2,9 +2,7 @@ function [var, cov] = exponentialVario(param, h, include_nugget)
 % This funparamtion returns the variogram and covariance for the exponential
 % variogram. 
 
-if nargin<3
-    include_nugget = 0; 
-end
+if nargin<3, include_nugget = 0; end
 
 if include_nugget==1
     var = param(1)*(1-exp(-h/param(2))) + param(3)*(h~=0);
